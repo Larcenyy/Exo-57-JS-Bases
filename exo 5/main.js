1630f15db3e642cafee6cfaedc7e253b240ad9e5
Rémy <remyc59440@hotmail.com>
2022-11-17 15:48:16 +0100

    exo 57

---
+++ exo 5/main.js
@@ -0,0 +1,7 @@
+let check = prompt("Saisissez un nombre à virgule");
+let check2 = prompt("Saisissez un second nombre à virgule");
+parseInt(check);
+
+let result = check * check2;
+
+alert(result);
---
+++ exo 6/main.js
@@ -0,0 +1,10 @@
+let age = prompt("quelle age avez vous?")
+
+
+
+
+if(age > 18){
+    alert("vous êtes bien majeur");
+
+}
+else alert("vous n'êtes pas majeur");
